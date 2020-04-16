acef2534f5dcfc43600421362b25b211a0a579e0
zero2603 <20153713@student.hust.edu.vn>
2020-04-17 00:31:34 +0700

    test v2

---
+++ test/precache-manifest.65022bb643a608c39ffbb5fe5b4e92c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979d7a6758428be4788d9db40cbe1167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ee4ebfd3dedbef2043",
+    "url": "/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "d3d63100b57943af09a8",
+    "url": "/static/css/main.b94aec53.chunk.css"
+  },
+  {
+    "revision": "25ee4ebfd3dedbef2043",
+    "url": "/static/js/2.ae744f4d.chunk.js"
+  },
+  {
+    "revision": "8819858dd618537df64ec6ab5c0d5c41",
+    "url": "/static/js/2.ae744f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d63100b57943af09a8",
+    "url": "/static/js/main.a0a897ea.chunk.js"
+  },
+  {
+    "revision": "e4538c8c508a2963a80d",
+    "url": "/static/js/runtime-main.d2c46e29.js"
+  },
+  {
+    "revision": "a526f250d09d5627d641239de8a1114b",
+    "url": "/static/media/logo.a526f250.png"
+  }
+]);
